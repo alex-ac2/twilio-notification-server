a8ccf2bbf273f7c6276e540ac2598b560709c66a
Alex Curci <alex.curci@gmail.com>
2019-09-10 22:17:04 -0400

    Update post route to accept notficationMessage and userPhoneNumber from request body

--- routes/notification.js
+++ routes/notification.js
@@ -7,18 +7,22 @@ module.exports = (client) => {
   router.post("/", (req, res) => {
     console.log("sessions POST route hit");
     console.log('REQUEST_BODY: ', req.body);
-    
-    client.messages
-      .create({
-        body: 'This is a new test message from your twillio notification server',
-        from: process.env.TWILIO_FROM_NUMBER,
-        to: process.env.TEST_NUMBER
-      })
-      .then(message => console.log(message.sid));
-    
-      console.log("Message sent...?");
 
-      res.sendStatus(200);
+    // notificationMessage & userPhoneNumber
+
+    if (req.body.hasOwnProperty('notificationMessage') && req.body.hasOwnProperty('userPhoneNumber')) {
+      client.messages
+        .create({
+          body: req.body.notificationMessage,
+          from: process.env.TWILIO_FROM_NUMBER,
+          to: req.body.userPhoneNumber
+        })
+        .then(message => console.log(message.sid));
+      
+        console.log("Message sent...?");
+  
+        res.sendStatus(200);
+      }
 
   });
 
